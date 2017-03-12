5400fc76bd764ee427883f0e09cd579f184baca3
Saravana Bhavan J <saravanabhavanj@gmail.com>
2017-03-12 23:10:53 +0530

    :tada: Initial commit

---
+++ HideLightbulbMargin/HideLightbulbMargin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace HideLightbulbMargin
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal class TestViewCreationListener : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            textView.Options.SetOptionValue("TextViewHost/SuggestionMargin", false);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Hide Lightbulb Margin
+A Visual Studio 2015/2015 addon that hides the editor margin that shows the "lightbulb" icon which reminds me of clippy.
+
+## Screenshots
+### Before
+![Before](Screenshots/before.png "Before")
+
+### After
+![After](Screenshots/after.png "After")
+
+You can still use the the "lightbulb" icon if you hover over the text.
